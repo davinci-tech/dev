0c6c1b7f2494926b8d1210c0128ca7edde33e770
davinci-tech <nstefanclaudel13@gmail.com>
2021-03-04 02:24:24 +0200

    a test for fin::seekg

--- Beta/tests/readFromStdioCpp/readFromStdioCpp.cpp
+++ Beta/tests/readFromStdioCpp/readFromStdioCpp.cpp
@@ -7,18 +7,13 @@ int main()
 
   int prevChar = 0;
   prevChar = fin.get();
-  prevChar = fin.get();
-  prevChar = fin.get();
 
-  fin >> prevChar;
   while (!fin.eof())
   {
-    if (fin.good())
-    {
-      prevChar = fin.get();
-      std::cout << prevChar;
-    }
+    prevChar = fin.get();
+    std::cout << (char)prevChar << ' ';
     fin.seekg(-2, fin.cur); // -2 because we want to read the previous char, -1 will move the position to the current char again
+    prevChar = fin.get();
   }
 
   return 0;

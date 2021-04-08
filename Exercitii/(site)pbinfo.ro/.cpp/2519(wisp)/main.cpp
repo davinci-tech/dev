4d8d440f9911e94b5cfeadb6c4abad91c9361439
davinci-tech <nstefanclaudel13@gmail.com>
2021-04-07 21:10:37 -0700

    new files

---
+++ Exercitii/(site)pbinfo.ro/.cpp/2519(wisp)/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+std::ifstream fin("wisp.in");
+std::ofstream fout("wisp.out");
+
+int main() {
+    size_t n, k, result;
+    fin >> n >> k;
+
+        
+
+    fout << result;
+
+    return 0;
+}

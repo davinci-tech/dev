6f6d85de21a8ba93ddce00646c53e729f9723e45
davinci-tech <nstefanclaudel13@gmail.com>
2021-04-23 08:31:10 +0300

    new files

---
+++ Proiecte/.py/Presentation/Chimie.DezechilibreEcologice/imports.py
@@ -0,0 +1,124 @@
+
+from manim import *
+
+TextDict = {
+    "Intro": {
+        "title": "Dezechilibrele Ecologice",
+        "definition": "= totalitatea schimbarilor ..."
+    }
+}
+
+class Grid(VGroup):
+    CONFIG = {
+        "height": 6.0,
+        "width": 6.0,
+    }
+
+    def __init__(self, rows, columns, **kwargs):
+        digest_config(self, kwargs, locals())
+        super().__init__(**kwargs)
+
+        x_step = self.width / self.columns
+        y_step = self.height / self.rows
+
+        for x in np.arange(0, self.width + x_step, x_step):
+            self.add(Line(
+                [x - self.width / 2., -self.height / 2., 0],
+                [x - self.width / 2., self.height / 2., 0],
+            ))
+        for y in np.arange(0, self.height + y_step, y_step):
+            self.add(Line(
+                [-self.width / 2., y - self.height / 2., 0],
+                [self.width / 2., y - self.height / 2., 0]
+            ))
+
+class ScreenGrid(VGroup):
+    CONFIG = {
+        "rows": 8,
+        "columns": 14,
+        "height": config.frame_y_radius * 2,
+        "width": 14,
+        "grid_stroke": 0.5,
+        "grid_color": WHITE,
+        "axis_color": RED,
+        "axis_stroke": 2,
+        "labels_scale": 0.25,
+        "labels_buff": 0,
+        "number_decimals": 2
+    }
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        rows = self.rows
+        columns = self.columns
+        grid = Grid(width=self.width, height=self.height, rows=rows, columns=columns)
+        grid.set_stroke(self.grid_color, self.grid_stroke)
+
+        vector_ii = ORIGIN + np.array((- self.width / 2, - self.height / 2, 0))
+        vector_si = ORIGIN + np.array((- self.width / 2, self.height / 2, 0))
+        vector_sd = ORIGIN + np.array((self.width / 2, self.height / 2, 0))
+
+        axes_x = Line(LEFT * self.width / 2, RIGHT * self.width / 2)
+        axes_y = Line(DOWN * self.height / 2, UP * self.height / 2)
+
+        axes = VGroup(axes_x, axes_y).set_stroke(self.axis_color, self.axis_stroke)
+
+        divisions_x = self.width / columns
+        divisions_y = self.height / rows
+
+        directions_buff_x = [UP, DOWN]
+        directions_buff_y = [RIGHT, LEFT]
+        dd_buff = [directions_buff_x, directions_buff_y]
+        vectors_init_x = [vector_ii, vector_si]
+        vectors_init_y = [vector_si, vector_sd]
+        vectors_init = [vectors_init_x, vectors_init_y]
+        divisions = [divisions_x, divisions_y]
+        orientations = [RIGHT, DOWN]
+        labels = VGroup()
+        set_changes = zip([columns, rows], divisions, orientations, [0, 1], vectors_init, dd_buff)
+        for c_and_r, division, orientation, coord, vi_c, d_buff in set_changes:
+            for i in range(1, c_and_r):
+                for v_i, directions_buff in zip(vi_c, d_buff):
+                    ubication = v_i + orientation * division * i
+                    coord_point = round(ubication[coord], self.number_decimals)
+                    label = Text(f"{coord_point}",font="Arial",stroke_width=0).scale(self.labels_scale)
+                    label.next_to(ubication, directions_buff, buff=self.labels_buff)
+                    labels.add(label)
+
+        self.add(grid, axes, labels)
+
+
+'''
+config.background_color = WHITE
+class MyPresentation(Scene):
+    def configure(self):
+        pass
+        # grid = ScreenGrid(); self.add(grid); self.play(ShowCreation(grid))
+    # use \tiny and \small in Latex for something like a TextBox
+
+    def construct(self):
+        self.configure()
+        self.Scene1() # The title and the definition
+        self.Scene2() # The future
+
+    def Scene1(self):
+        title = Text("Poluarea", color = BLACK)
+        definition = Text(
+"""este procesul prin care mediul natural este impurificat cu substanţe nocive care afectează 
+                                            sănătatea vieţuitoarelor de pe planetă""",
+            width = config.frame_width - 1,
+            height = config.frame_height - 2,
+            color = BLACK,
+            rate_func = not_quite_there
+        )
+        self.play(Write(title))
+        self.wait(2)
+        self.play(Transform(title, definition))
+        self.wait(3)
+
+        self.remove(definition)
+        self.play(title.animate.to_edge(DOWN, buff = -4))
+
+    def Scene2(self):
+        pass
+'''
